f0d5979728fc65a20f2d6922eafc4e896d610bb5
Nicholas Tow <nxtr1114@gmail.com>
2024-04-12 21:46:51 -0400

    move hardcodes to config

--- src/swag/cleanlogin/Main.java
+++ src/swag/cleanlogin/Main.java
@@ -11,8 +11,14 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class Main extends JavaPlugin implements Listener {
 
+    private int teleportDelay; // Seconds to delay before teleport
+    private int safeLocationRange; // Range for finding a safe location
+
     @Override
     public void onEnable() {
+        this.saveDefaultConfig(); // Ensure config.yml is saved to plugin folder if not existing
+        teleportDelay = this.getConfig().getInt("settings.teleportDelay");
+        safeLocationRange = this.getConfig().getInt("settings.safeLocationRange");
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -23,42 +29,37 @@ public class Main extends JavaPlugin implements Listener {
         new BukkitRunnable() {
             @Override
             public void run() {
-                // Make sure player is still online
                 if(event.getPlayer().isOnline()) {
                     Location safeLocation = findSafeLocation(event.getPlayer().getLocation());
                     if (safeLocation != null) {
-                        event.getPlayer().teleport(safeLocation); // Teleport to a safe location
+                        event.getPlayer().teleport(safeLocation);
                     } else {
-                        // If no safe location is found, teleport player to the world spawn
                         event.getPlayer().teleport(event.getPlayer().getWorld().getSpawnLocation());
                     }
-                    event.getPlayer().setGameMode(GameMode.SURVIVAL);  // Change back to survival mode
+                    event.getPlayer().setGameMode(GameMode.SURVIVAL);
                 }
             }
-        }.runTaskLater(this, 20L * 5);  // 20 Ticks * 5 = 5 seconds
+        }.runTaskLater(this, 20L * teleportDelay);
     }
 
-    // Method to find a safe location near the player's current location
     public Location findSafeLocation(Location location) {
-        int range = 5; // Range to check for a safe location
-        for (int x = -range; x <= range; x++) {
-            for (int y = -range; y <= range; y++) {
-                for (int z = -range; z <= range; z++) {
+        for (int x = -safeLocationRange; x <= safeLocationRange; x++) {
+            for (int y = -safeLocationRange; y <= safeLocationRange; y++) {
+                for (int z = -safeLocationRange; z <= safeLocationRange; z++) {
                     Location checkLocation = location.clone().add(x, y, z);
                     if (isLocationSafe(checkLocation)) {
-                        return checkLocation; // Found a safe location
+                        return checkLocation;
                     }
                 }
             }
         }
-        return null; // No safe location found
+        return null;
     }
-
-    // Method to check if a location is safe (not inside blocks)
     public boolean isLocationSafe(Location location) {
         Block feet = location.getBlock();
         Block head = feet.getRelative(0, 1, 0);
         Block ground = feet.getRelative(0, -1, 0);
         return !feet.getType().isSolid() && !head.getType().isSolid() && ground.getType().isSolid();
     }
+    // The isLocationSafe method remains unchanged
 }
